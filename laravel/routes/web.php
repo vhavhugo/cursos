33fee59a8731fdf678107545d19f69ca9a8cb88c
victor_hugo_valle <vhavhugo@gmail.com>
2018-11-16 22:36:45 -0200

    Rotas básicas - Métodos que permitem mais de um verbo

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -23,15 +23,28 @@ Route::prefix('treinaweb/clients')->group(function(){
     
     Route::get('create/new', function(){
         $html = '<h1>Criar Cliente</h1>';
+
+        $html .= '<br>
+            <form action="' . route('clients.save') . '" method="POST">
+                <input type="hidden" name="_token" value="' . csrf_token() . '">
+                Nome: <input type="text" name="name"><br>
+                Idade: <input type="text" name="age"><br>
+                <input type="submit" value="Salvar">
+            </form>
+            ';
         $html .= '<br><a href="' . route('clients.list') . '">Lista de Clientes</a>';
         return $html;
     });
     
-    Route::get('{name}/{age?}', function($name, $age='não definido'){
-        $html = "Detalhes do cliente {$name} ele tem {$age} anos";
-        $html .= '<br><a href="' . route('clients.list') . '">Lista de Clientes</a>';
-        return $html;
-    })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);
+    Route::match(['post','put'], 'save', function(){
+        return 'Cliente criado com sucesso!';
+    })->name('clients.save');
+
+    // Route::get('{name}/{age?}', function($name, $age='não definido'){
+    //     $html = "Detalhes do cliente {$name} ele tem {$age} anos";
+    //     $html .= '<br><a href="' . route('clients.list') . '">Lista de Clientes</a>';
+    //     return $html;
+    // })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);
     
 });
 
